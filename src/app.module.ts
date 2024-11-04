abab7303d41dedf69c84781ce4cb93bb289bd514
Long <longnp@btc-studio.io>
2024-11-05 02:32:57 +0900

    initial base modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from 'src/modules/user/user.module';
-import { AppController } from './app.controller';
-import { FileModule } from './modules/asset/file.module';
-import { CommonModule } from './common/common.module';
-import { NotificationModule } from './modules/notification/notification.module';
+import { CommonModule } from 'src/common/common.module';
+import { UserModule } from 'src/user-management/user.module';
+import { AppController } from 'src/app.controller';
+import { FileModule } from 'src/storage/file.module';
+import { NotificationModule } from 'src/notification/notification.module';
 
 @Module({
   imports: [
